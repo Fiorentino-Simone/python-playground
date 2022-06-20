ebd1d9bcc71864ece924bed6e1b6fddc2dc8f111
Simone Fiorentino <simone.fiorentino@cambieri.it>
2022-06-20 21:27:11 +0200

    See jupyter and how manage data with pandas

---
+++ EXERCISE/Ese09 Jupyter/index.py
@@ -0,0 +1,8 @@
+#jupyter Notebook
+#per installarlo usare: pip3.10 install jupyter
+#per usarlo usare: jupyter notebook --> IN UN TERMINALE NELLA CARTELLA DI LAVORO, NON nel file interno 
+
+#esso serve per andare a lavorare con molti dati 
+#ed esso viene usato sia come shell interattiva che per eseguire/salvare codice
+
+
